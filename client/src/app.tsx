8429fb67ee88a8814d7a154cb9f10d54fd7b3090
im-calvin <kelvinwong0519@gmail.com>
2023-03-08 23:30:38 -0800

    typescript is wack

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,20 +1,16 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 const App = () => {
   return (
-    <>    
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
     </>
-  )
-}
+  );
+};
 
-export default App;
-
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(
-  <App/>
-);
+root.render(<App />);
