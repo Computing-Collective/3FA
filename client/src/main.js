ce271222ee319f67378add1bd432a7ee37245214
im-calvin <kelvinwong0519@gmail.com>
2023-03-16 22:04:27 -0700

    disable context-isolation by exposing API_ENDPOINT channel

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 const { desktopCapturer } = require("electron");
 require("dotenv").config();
@@ -15,10 +15,11 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
-      nodeIntegration: true, // TODO contextIsolation
-      contextIsolation: false,
     },
   });
+
+  // send API_ENDPOINT to renderer on the "API_ENDPOINT" channel
+  mainWindow.webContents.send("API_ENDPOINT", process.env.API_ENDPOINT);
 
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
@@ -30,7 +31,9 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", () => {
+  createWindow();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
--- client/src/preload.js
+++ client/src/preload.js
@@ -1,35 +1,9 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 // In the preload script.
-const { ipcRenderer } = require("electron");
+const { ipcRenderer, contextBridge } = require("electron");
 
-ipcRenderer.on("SET_SOURCE", async (event, sourceId) => {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: {
-        mandatory: {
-          chromeMediaSource: "desktop",
-          chromeMediaSourceId: sourceId,
-          minWidth: 1280,
-          maxWidth: 1280,
-          minHeight: 720,
-          maxHeight: 720,
-        },
-      },
-    });
-    handleStream(stream);
-  } catch (e) {
-    handleError(e);
-  }
+contextBridge.exposeInMainWorld("internal", {
+  getAPIEndpoint: (callback) =>
+    ipcRenderer.on("API_ENDPOINT", (event, arg) => callback(arg)),
 });
-
-function handleStream(stream) {
-  const video = document.querySelector("video");
-  video.srcObject = stream;
-  video.onloadedmetadata = (e) => video.play();
-}
-
-function handleError(e) {
-  console.log(e);
-}
