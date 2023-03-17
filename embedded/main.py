2c5213f8d688b1aa7419ce70f62d26f16ce31db4
Matthew Chow <matthewchow03@gmail.com>
2023-03-17 14:37:44 -0700

    Full filtering based on IMU charaterization

--- embedded/main.py
+++ embedded/main.py
@@ -36,6 +36,10 @@ from digitalio import DigitalInOut, Direction, Pull
 import busio
 from adafruit_lsm6ds.lsm6ds33 import LSM6DS33
 
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# INIT
+# --------------------------------------------------------------------------------------------------------------------------------------------
+
 # Push button start stop
 start_btn = DigitalInOut(board.GP28)    # upper
 start_btn.direction = Direction.INPUT
@@ -65,10 +69,14 @@ sensor = LSM6DS33(i2c)
 
 
 # Global parameters
-sensitivity = 16
+sensitivity = 20
 buffer_offset = 5 # there are typically 3 elements of feedback 
                   # for example forward move is [20, -20, -18, -12, -4, 0, 0, 0]
 z_offset = 10 # don't use z_offset on raw data (flip z needs to be not around 0 to detect flips as the sign of the number)
+
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# ADDITIONAL FUNCTIONS
+# --------------------------------------------------------------------------------------------------------------------------------------------
 
 def print_all_imu():
     # \x1b[2J clear the screen
@@ -105,6 +113,8 @@ def init_hardware():
     
     init = True
 
+
+
 def sequence_correct_led():
     for i in range(5):
         correct_led.value = True    
@@ -114,6 +124,10 @@ def sequence_correct_led():
     
     # Reset the correct LED after blinking
     correct_led.value = False
+
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# IMU DATA PARSING -> SEQUENCE
+# --------------------------------------------------------------------------------------------------------------------------------------------
 
 # Return a list of all the valid moves that happened in the sequence
 # Note sequence is a dict of <string, list> where all lists are the same length
@@ -235,7 +249,7 @@ def check_sequence(sequence):
             if y > sensitivity / 2:
                 # ignore the next buffer_offset elements in list
                 buffer = buffer + buffer_offset
-            elif y < -1 * sensitivity:
+            elif y < -1 * sensitivity - 2:  # manually increase by 2 because -Y seems to be sensitive
                 valid_moves_indexed.append(("BACKWARDS-MOVE", i))
                 buffer = buffer + buffer_offset
 
@@ -256,7 +270,9 @@ def check_sequence(sequence):
                 valid_moves_indexed.append(("DOWN-MOVE", i))  
                 buffer = buffer + buffer_offset
 
-
+    # --------------------------------------------------------------------------------------------------------------------------------------------
+    # SEQUENCE PROCESSING
+    # --------------------------------------------------------------------------------------------------------------------------------------------
 
     # Processing sequence
     # Sort based on time -> filter moves caused by feedback -> put in list of moves
@@ -269,10 +285,14 @@ def check_sequence(sequence):
     # We now have to filter the overlapping moves that shouldn't exist
     # This depends on a precedence
     # For example, UP-DOWN-FLIP can also triggers other moves if the flip is aggressive
-    # We do this be removing any neighbouring moves that occured within +/-0.2ms of the flip
-    tolerance = 2
+    # We do this be removing any neighbouring moves that occured within +/-0.3ms of the flip
+    tolerance = 3
+
+    # Note the filters occur in order based on precedence
+    # Flip, up/down, left/right (these compound)
 
     # Filter so that flip takes precedence over all moves ----------------------------------------------------------
+    print("flip filter begin")
     flip_occurence_indices = []
     for pair in valid_moves_indexed:
         if pair[0] == "UP-DOWN-FLIP":
@@ -293,22 +313,44 @@ def check_sequence(sequence):
 
 
     # Filter so that up down takes precedence over other moves ----------------------------------------------------------
-    # up_down_occurence_indices = []
-    # for pair in valid_moves_indexed:
-    #     if pair[0] == "UP-DOWN-FLIP":
-    #         up_down_occurence_indices.append(pair[1])
+    print("up/down filter begin")
+    up_down_occurence_indices = []
+    for pair in valid_moves_indexed:
+        if pair[0] == "UP-MOVE" or pair[0] == "DOWN-MOVE":
+            up_down_occurence_indices.append(pair[1])
 
-    # # print("flip indices", flip_occurence_indices)
+    # print("flip indices", flip_occurence_indices)
 
-    # # Remove moves from list based on filter
-    # for index in up_down_occurence_indices:
-    #     for move_index, pair in enumerate(valid_moves_indexed):
-    #         # first condition checks if move is within tolerance * 0.1ms of the flip
-    #         if abs(pair[1] - index) < tolerance  and (pair[0] != "UP-MOVE" or pair[0] != "DOWN-MOVE"):
-    #             # print("try to remove", pair, "at move index", move_index)
-    #             del valid_moves_indexed[move_index]
+    # Remove moves from list based on filter
+    for index in up_down_occurence_indices:
+        for move_index, pair in enumerate(valid_moves_indexed):
+            # first condition checks if move is within tolerance * 0.1ms of the flip
+            if abs(pair[1] - index) < tolerance  and pair[0] != "UP-MOVE" and pair[0] != "DOWN-MOVE":
+                # print("try to remove", pair, "at move index", move_index)
+                print("removing", pair)
+                del valid_moves_indexed[move_index]
 
-    # print("up/down filtered:", valid_moves_indexed, "\n\n")
+    print("up/down filtered:", valid_moves_indexed, "\n\n")
+
+    # Filter so that left right takes precedence over front back (more coupled baesd on analysis of data) -----------------------------------
+    print("left/right filter begin")
+    left_right_occurence_indices = []
+    for pair in valid_moves_indexed:
+        if pair[0] == "LEFT-MOVE" or pair[0] == "RIGHT-MOVE":
+            left_right_occurence_indices.append(pair[1])
+
+    # print("flip indices", flip_occurence_indices)
+
+    # Remove moves from list based on filter
+    for index in left_right_occurence_indices:
+        for move_index, pair in enumerate(valid_moves_indexed):
+            # first condition checks if move is within tolerance * 0.1ms of the flip
+            if abs(pair[1] - index) < tolerance  and pair[0] != "LEFT-MOVE" and pair[0] != "RIGHT-MOVE":
+                # print("try to remove", pair, "at move index", move_index)
+                print("removing", pair)
+                del valid_moves_indexed[move_index]
+
+    print("left/right filtered:", valid_moves_indexed, "\n\n")
 
 
 
@@ -316,14 +358,15 @@ def check_sequence(sequence):
     for move in valid_moves_indexed:
         sorted_moves.append(move[0])
 
-    print(sorted_moves)
+    print("final sequence:", sorted_moves)
     print()
 
     return sorted_moves
 
 
-
-
+# --------------------------------------------------------------------------------------------------------------------------------------------
+# MAIN LOGIC
+# --------------------------------------------------------------------------------------------------------------------------------------------
 
 # Sequence variable
 sequence = {"AX" : [],
@@ -440,6 +483,11 @@ while True:
 
     There is speed up and slow down
     - Identify which came first
+
+
+
+    Note giving a slight smooth motion in the opposite direciton of where you want to go before doing the motion
+    Can get a stronger signal since the acceleration will be relatively greater
     
     
     '''
