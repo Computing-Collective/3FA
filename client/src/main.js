3499a4cae36ee3211a447f347fc05af4b3dd1750
im-calvin <kelvinwong0519@gmail.com>
2023-04-04 13:23:40 -0700

    rm inspect

--- client/src/main.js
+++ client/src/main.js
@@ -30,8 +30,6 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools(); // TODO rm
 };
 
 // This method will be called when Electron has finished
