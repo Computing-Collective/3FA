c770fd6c120c8d49146bc91dcd3fcc05acabe709
im-calvin <kelvinwong0519@gmail.com>
2023-04-04 01:27:27 -0700

    make cam smaller + center cam

--- client/src/pages/Camera.jsx
+++ client/src/pages/Camera.jsx
@@ -27,7 +27,7 @@ export function Camera() {
 
   return (
     <>
-      <div className="wid flex w-3/4 flex-col text-center">
+      <div className="wid flex w-2/3 flex-col items-center text-center">
         {error !== "" && <DisplayError text={error} />}
         <h1 className="m-2">Smile for the camera</h1>
         <Video
